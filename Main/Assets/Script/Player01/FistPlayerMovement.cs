570f04dfa63e8f7b7c4e7d8a7c545078b95670d7
jasonben677 <31059981+jasonben677@users.noreply.github.com>
2020-03-13 12:00:40 +0800

    model test

---
+++ Main/Assets/Script/Player01/FistPlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistPlayerMovement : MonoBehaviour
+{
+    Rigidbody myRigi;
+    Camera myCam;
+
+    private void Awake()
+    {
+        myRigi = transform.GetComponent<Rigidbody>();
+        myCam = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        //float moveH = (Input.GetKey(KeyCode.D) ? 1.0f : 0f) - (Input.GetKey(KeyCode.A) ? 1.0f : 0f);
+        //float moveV = (Input.GetKey(KeyCode.W) ? 1.0f : 0f) - (Input.GetKey(KeyCode.S) ? 1.0f : 0f);
+
+        //Vector3 move = myCam.transform.right * moveH + myCam.transform.forward * moveV;
+        //transform.forward = move.normalized;
+        //myRigi.velocity = move;
+
+    }
+
+}
