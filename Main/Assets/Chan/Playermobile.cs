513d799bf07541746312e00ee3e87474c5678a96
SlimeKaree <z98132968@gmail.com>
2020-03-13 16:34:47 +0800

    Player測試

---
+++ Main/Assets/Chan/Playermobile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermobile : MonoBehaviour
+{
+
+    public Transform Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fX=Input.GetAxis("Horizontal");
+        float fY=Input.GetAxis("Vertical");
+        float fZ = Input.GetAxis("Jump");
+
+        Vector3 vRight = Player.right;
+        Vector3 vFor = Player.forward;
+        Player.position = Player.position + (fX * vRight + fY * vFor) * Time.deltaTime;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Player.position, Player.position + Player.forward * 2);
+    }
+}
