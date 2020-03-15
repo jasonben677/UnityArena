e2888901e7330549a8d7ee6590b76d7e0de14e10
SlimeKaree <z98132968@gmail.com>
2020-03-15 22:44:12 +0800

    計算轉向角度,腳色撞牆會抖動,計算公式還未整理好

--- Main/Assets/Chan/Playermobile.cs
+++ Main/Assets/Chan/Playermobile.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Playermobile : MonoBehaviour
 {
-
+    public Animator PlayAni;
     public Transform Player;
+    private void Awake()
+    {
+        
+    }
     void Start()
     {
         
@@ -14,14 +18,50 @@ public class Playermobile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float fX=Input.GetAxis("Horizontal");
-        float fY=Input.GetAxis("Vertical");
-        float fZ = Input.GetAxis("Jump");
-        ThePlayermove(fX, fY, fZ);
-        
+        float fY = Input.GetAxis("Vertical");
+        float fX = Input.GetAxis("Horizontal");
+
+        if (InputPlayrRun())
+        {
+            PlayAni.Play("Run");
+            ThePlayermove(fX * 2, fY * 2, 0);
+
+        }
+        else if (InputPlayerWalk())
+        {
+            PlayAni.Play("Walk");
+            ThePlayermove(fX, fY, 0);
+        }
+        else { PlayAni.Play("Idel"); }
+
     }
 
+    //判斷是否按下走路的移動
+    private bool InputPlayerWalk()
+    {
+        if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.S) |
+              Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.D))
+        {
+            return true;
+        }
+        else       
+        return false;
+        
+    }
+    private bool InputPlayrRun() 
+    {
+        if ((Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.W)) |
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.S)) | 
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.A)) | 
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.D)))
+        { 
+            return true;
+        }
+        else 
+            return false;
+    }
 
+    //計算腳色移動速度
     private void ThePlayermove(float x,float y,float z) 
     {
 
@@ -31,19 +71,37 @@ public class Playermobile : MonoBehaviour
         Vector3 vFor = Player.forward;
 
         Vector3 vm = x * vRight + y * vFor;
-        Player.position = Player.position + vm * Time.deltaTime;
+        Player.position = Player.position + vm *2.0f* Time.deltaTime;
 
         //Jump未來新增暫時還想不到
         //Vector3 vJump = Player.;
 
     }
+    //腳色的轉向角度
 private void NpcRotation()
     {
-        
+        Vector3 pos = Player.position;
+        Vector3.Dot(Player.position,pos);
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawLine(Player.position, Player.position + Player.forward * 2);
+    }
+    private void OnMouseEnter()
+    {
+        
+    
+    
+        float fY = Input.GetAxis("Vertical");
+        float fX = Input.GetAxis("Horizontal");
+        Vector3 vRight = Player.right;
+        Vector3 vFor = Player.forward;
+
+        Vector3 vm = fX * vRight + fY * vFor;
+        Vector3 pos = Player.position;
+        Vector3 nextPlayerpos = Player.position + vm * 2.0f;
+        float pDot = Vector3.Dot(Player.position, nextPlayerpos);
+        Debug.Log(pDot);
     }
 }
