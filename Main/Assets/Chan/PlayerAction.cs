a5630929e465c296ef764dc6cc5e1ed3375e4554
SlimeKaree <z98132968@gmail.com>
2020-03-16 21:09:29 +0800

    腳色的夾角計算

---
+++ Main/Assets/Chan/PlayerAction.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction 
+{
+    Playermobile m_Playermoble = new Playermobile();
+    public  enum Anim
+    {
+    Idel,
+    Walk,
+    Run,
+    Jump,
+    Attack1,
+    Attack2,
+    Attack3
+    }
+
+    public bool InputPlayerWalk()
+    {
+        if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.S) |
+              Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.D))
+        {
+            return true;
+        }
+        else
+            return false;
+
+    }
+    public bool InputPlayrRun()
+    {
+        if ((Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.W)) |
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.S)) |
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.A)) |
+            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.D)))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    //計算腳色移動速度
+    
+
+
+
+
+
+
+
+}
--- Main/Assets/Chan/Playermobile.cs
+++ Main/Assets/Chan/Playermobile.cs
@@ -6,13 +6,15 @@ public class Playermobile : MonoBehaviour
 {
     public Animator PlayAni;
     public Transform Player;
+    public float MoveSeed=2.0f;
+    PlayerAction m_PlayerAction;
     private void Awake()
     {
-        
     }
     void Start()
     {
-        
+        m_PlayerAction = new PlayerAction();
+
     }
 
     // Update is called once per frame
@@ -20,49 +22,27 @@ public class Playermobile : MonoBehaviour
     {
         float fY = Input.GetAxis("Vertical");
         float fX = Input.GetAxis("Horizontal");
+        NpcRotation(fX,fY);
 
-        if (InputPlayrRun())
+        if (m_PlayerAction.InputPlayrRun())
         {
+            MoveSeed = 4.0f;
             PlayAni.Play("Run");
-            ThePlayermove(fX * 2, fY * 2, 0);
-
+           ThePlayermove(fX , fY , 0);
+        
         }
-        else if (InputPlayerWalk())
+        else if (m_PlayerAction.InputPlayerWalk())
         {
+            MoveSeed = 2.0f;
             PlayAni.Play("Walk");
             ThePlayermove(fX, fY, 0);
         }
         else { PlayAni.Play("Idel"); }
-
-    }
-
-    //判斷是否按下走路的移動
-    private bool InputPlayerWalk()
-    {
-        if (Input.GetKey(KeyCode.W) | Input.GetKey(KeyCode.S) |
-              Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.D))
-        {
-            return true;
-        }
-        else       
-        return false;
+     
         
     }
-    private bool InputPlayrRun() 
-    {
-        if ((Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.W)) |
-            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.S)) | 
-            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.A)) | 
-            (Input.GetKey(KeyCode.LeftShift) & Input.GetKey(KeyCode.D)))
-        { 
-            return true;
-        }
-        else 
-            return false;
-    }
 
-    //計算腳色移動速度
-    private void ThePlayermove(float x,float y,float z) 
+    public void ThePlayermove(float x, float y, float z)
     {
 
 
@@ -71,18 +51,59 @@ public class Playermobile : MonoBehaviour
         Vector3 vFor = Player.forward;
 
         Vector3 vm = x * vRight + y * vFor;
-        Player.position = Player.position + vm *2.0f* Time.deltaTime;
+        Player.position = Player.position + vm * MoveSeed * Time.deltaTime;
 
-        //Jump未來新增暫時還想不到
+        //Jump Z軸的位移，加上重力
         //Vector3 vJump = Player.;
 
     }
-    //腳色的轉向角度
-private void NpcRotation()
+
+
+
+
+
+    //判斷角色Ray是否碰牆,如果有moveseed=0
+    public void  PlayerRay()
     {
-        Vector3 pos = Player.position;
-        Vector3.Dot(Player.position,pos);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10)) 
+        {
+            print(hit.point);
+            print(hit.transform.position);
+            print(hit.collider.gameObject);
+            float PlayerHitDis = Vector3.Dot(Player.position, hit.transform.position);
+        Debug.DrawLine(hit.transform.position,
+            hit.transform.position+hit.transform.forward*3,Color.red);
+
+        }
+
     }
+
+
+    
+    //腳色的轉向角度
+private void NpcRotation(float Input_V ,  float Input_H)
+    {
+        float Sum_Angle = Mathf.Atan(Input_V/Input_H)/(Mathf.PI/180);
+
+
+        Sum_Angle = Input_V < 0 ? Sum_Angle + 180 : Sum_Angle;
+
+
+
+        if (float.IsNaN(Sum_Angle))
+            Sum_Angle = 0;
+
+        Debug.Log(Sum_Angle);
+    
+    
+    
+    
+    
+    
+    }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
@@ -90,18 +111,14 @@ private void NpcRotation()
     }
     private void OnMouseEnter()
     {
-        
-    
-    
-        float fY = Input.GetAxis("Vertical");
-        float fX = Input.GetAxis("Horizontal");
-        Vector3 vRight = Player.right;
-        Vector3 vFor = Player.forward;
-
-        Vector3 vm = fX * vRight + fY * vFor;
+               
         Vector3 pos = Player.position;
-        Vector3 nextPlayerpos = Player.position + vm * 2.0f;
-        float pDot = Vector3.Dot(Player.position, nextPlayerpos);
-        Debug.Log(pDot);
+       
+        float fAng=Vector3.Angle(pos,Player.forward);
+        
+        Debug.Log(fAng);
+
     }
+
+    
 }
