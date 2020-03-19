5135ee45d6c335c32c1dd9cd63c1ddaae904f0f2
jasonben677 <31059981+jasonben677@users.noreply.github.com>
2020-03-19 11:22:38 +0800

    test

--- Main/Assets/Script/Player01/FistPlayerMovement.cs
+++ Main/Assets/Script/Player01/FistPlayerMovement.cs
@@ -15,13 +15,16 @@ public class FistPlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //float moveH = (Input.GetKey(KeyCode.D) ? 1.0f : 0f) - (Input.GetKey(KeyCode.A) ? 1.0f : 0f);
-        //float moveV = (Input.GetKey(KeyCode.W) ? 1.0f : 0f) - (Input.GetKey(KeyCode.S) ? 1.0f : 0f);
-
-        //Vector3 move = myCam.transform.right * moveH + myCam.transform.forward * moveV;
-        //transform.forward = move.normalized;
-        //myRigi.velocity = move;
-
+        float moveH = (Input.GetKey(KeyCode.D) ? 1.0f : 0f) - (Input.GetKey(KeyCode.A) ? 1.0f : 0f);
+        float moveV = (Input.GetKey(KeyCode.W) ? 1.0f : 0f) - (Input.GetKey(KeyCode.S) ? 1.0f : 0f);
+        if (moveH == 0 && moveV == 0)
+        {
+            return;
+        }
+        Vector3 move = (myCam.transform.forward * moveV) + (myCam.transform.right * moveH);
+        move.y = 0;
+        myRigi.velocity = move;
+        transform.forward = move.normalized;
     }
 
 }
