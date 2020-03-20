c2b43ae04605c4bd4511b4dcb96bdfdf3a4fcfcb
jasonben677 <31059981+jasonben677@users.noreply.github.com>
2020-03-20 16:34:24 +0800

    腳色和攝影機大致設定

---
+++ Main/Assets/Script/Player01/FistPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistPlayerController : MonoBehaviour
+{
+    [SerializeField] GameObject camControl;
+    FistPlayerMovement playerMovement;
+
+    Animator playerAnim;
+    Rigidbody playerRigi;
+
+    private void Awake()
+    {
+        playerAnim = GetComponent<Animator>();
+        playerRigi = GetComponent<Rigidbody>();
+
+        playerMovement = new FistPlayerMovement();
+        playerMovement.SetPlayerComponent(playerAnim, playerRigi, gameObject, camControl);
+    }
+
+    private void FixedUpdate()
+    {
+        playerMovement.PlayerMove(2.5f);
+        playerMovement.CameraMove(1.5f);
+    }
+
+}
--- Main/Assets/Script/Player01/FistPlayerMovement.cs
+++ Main/Assets/Script/Player01/FistPlayerMovement.cs
@@ -2,29 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FistPlayerMovement : MonoBehaviour
+public class FistPlayerMovement
 {
-    Rigidbody myRigi;
-    Camera myCam;
+    Animator _playerAnim;
+    Rigidbody _playerRigi;
+    GameObject _player;
+    GameObject myCam;
 
-    private void Awake()
+    float go;
+    public float walkSign;
+
+
+    public void SetPlayerComponent(Animator _animator, Rigidbody _rigidbody, GameObject _playerObj, GameObject _camControl)
     {
-        myRigi = transform.GetComponent<Rigidbody>();
-        myCam = Camera.main;
+        _playerAnim = _animator;
+        _playerRigi = _rigidbody;
+        _player = _playerObj;
+        myCam = _camControl;
     }
 
-    private void FixedUpdate()
+    /// <summary>
+    /// 腳色移動
+    /// </summary>
+    public void PlayerMove(float _moveSpeed)
     {
         float moveH = (Input.GetKey(KeyCode.D) ? 1.0f : 0f) - (Input.GetKey(KeyCode.A) ? 1.0f : 0f);
         float moveV = (Input.GetKey(KeyCode.W) ? 1.0f : 0f) - (Input.GetKey(KeyCode.S) ? 1.0f : 0f);
+
+        Vector2 speed = new Vector2(moveH, moveV);
+        walkSign = Mathf.SmoothDamp(walkSign, speed.magnitude, ref go, 0.3f);
+        //float length = speed.magnitude;
+        _playerAnim.SetFloat("Speed", walkSign);
+
         if (moveH == 0 && moveV == 0)
         {
             return;
         }
+
         Vector3 move = (myCam.transform.forward * moveV) + (myCam.transform.right * moveH);
         move.y = 0;
-        myRigi.velocity = move;
-        transform.forward = move.normalized;
+        _playerRigi.velocity = move * _moveSpeed;
+        _player.transform.forward = move.normalized;
     }
 
+
+    public void CameraMove(float _moveSpeed)
+    {
+        Vector3 pos = new Vector3(_player.transform.position.x, myCam.transform.position.y, _player.transform.position.z);
+        myCam.transform.position = pos;
+    }
 }
