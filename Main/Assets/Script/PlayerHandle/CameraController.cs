43e5b4a999a8d8ae7b2be5e01fc80f6f11aa12d8
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-03-25 13:39:41 +0800

    camera control
    camera水平旋轉時，model會跟著旋轉(還在調整中)

---
+++ Main/Assets/Script/PlayerHandle/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public PlayerInput pi;
+    public float horizontalSpeed = 100.0f;
+    public float verticalSpeed = 80.0f;
+
+    private GameObject playerHandle;
+    private GameObject cameraHandle;
+    private float tempEulerX;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cameraHandle = transform.parent.gameObject;
+        playerHandle = cameraHandle.transform.parent.gameObject;
+        tempEulerX = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerHandle.transform.Rotate(Vector3.up, pi.JRight * horizontalSpeed * Time.deltaTime);
+        tempEulerX -= pi.JUp * verticalSpeed * Time.deltaTime;
+        tempEulerX = Mathf.Clamp(tempEulerX, - 30, 40);
+        cameraHandle.transform.localEulerAngles = new Vector3(tempEulerX, 0, 0);
+    }
+}
