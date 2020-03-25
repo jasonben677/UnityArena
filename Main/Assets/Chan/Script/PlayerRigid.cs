89c5f00585d4b5666306f5ca875a9e415f26079e
SlimeKaree <z98132968@gmail.com>
2020-03-25 16:24:40 +0800

    動畫切換完成

--- Main/Assets/Chan/Script/PlayerRigid.cs
+++ Main/Assets/Chan/Script/PlayerRigid.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerRigid : MonoBehaviour
 {
-    public Rigidbody Player;
+    public CharacterController Player;
     public float speed = 2.0f;
     public Animator Ani;
     string  sHorizontal = "Horizontal";
     string sVertical = "Vertical";
-   
+    float fSpeed = 0;
+
    
 
     // Start is called before the first frame update
     void Start()
     {
       
-        this.Player = GetComponent<Rigidbody>();
+        this.Player = GetComponent<CharacterController>();
+        this.Ani = this.GetComponent<Animator>();
+        Ani.SetFloat("Walk", fSpeed);
     }
 
     // Update is called once per frame
@@ -37,21 +40,31 @@ public class PlayerRigid : MonoBehaviour
         {
             PlayerAtcion(PLAYERACTION.Run);
             speed = 4f;
-            PlayerInstantSpeed();
 
+            PlayerInstantSpeed();
+            Debug.Log("Run");
         }
         else if (InputW | InputD | InputA | InputS)
         {
             PlayerAtcion(PLAYERACTION.Walk);
             speed = 2f;
             PlayerInstantSpeed();
+            Debug.Log("walk");
 
+            
+        }
+        else if (Input.GetKey(KeyCode.Space)) 
+        {
+            PlayerAtcion(PLAYERACTION.Jump);
+            
 
         }
-        else 
+        else
         {
             speed = 0;
-            PlayerAtcion(PLAYERACTION.Idel); 
+            PlayerAtcion(PLAYERACTION.Idle);
+            
+            Debug.Log("idle");
         }
     }
     
@@ -60,35 +73,35 @@ public class PlayerRigid : MonoBehaviour
 
     private void PlayerAtcion(PLAYERACTION m_playeraction)
     {
-
+        _PlayerAtcion = m_playeraction;
 
         switch (_PlayerAtcion)
         {
-            case PLAYERACTION.Idel:
-                Ani.Play("Idel");
+            case PLAYERACTION.Idle:
+                if((fSpeed -= Time.deltaTime)<0)fSpeed=0;
+                Ani.SetFloat("Walk", fSpeed);
                 break;
             case PLAYERACTION.Walk:
-                Ani.Play("Walk");
+                if((fSpeed+= Time.deltaTime)>0.5f)fSpeed=0.5f;
+                Ani.SetFloat("Walk", fSpeed);
+
                 break;
 
             case PLAYERACTION.Run:
-                Ani.Play("Run");
+                if((fSpeed+= Time.deltaTime)>1f)fSpeed=1f;
+                Ani.SetFloat("Walk", fSpeed);
                 break;
 
             case PLAYERACTION.Jump:
-                Ani.Play("Jump");
                 break;
 
             case PLAYERACTION.Attack1:
-                Ani.Play("Attack1");
                 break;
 
             case PLAYERACTION.Attack2:
-                Ani.Play("Attack2");
                 break;
 
             case PLAYERACTION.Attack3:
-                Ani.Play("Attack3");
                 break;
 
         }
@@ -97,27 +110,28 @@ public class PlayerRigid : MonoBehaviour
     {
         float InpVertical = Input.GetAxis(sVertical);
         float InpHorizontal = Input.GetAxis(sHorizontal);
+
+
         Vector3 direction = new Vector3(InpHorizontal, 0f, InpVertical);
+        direction = transform.TransformDirection(direction);
 
-        if (Input.anyKey)
-        {
 
-            Player.velocity = speed * direction;
-
-        }
+        direction *= speed;
+  
+        Player.Move(direction * Time.deltaTime);
+        
     }
+
     //0正前方90右方180後方270左方
     private void PlayerRotate()
     {
-        Quaternion qRotat= this.transform.rotation;
-
-        Debug.Log(qRotat);
+       
     }
 
 
     private enum PLAYERACTION
     {
-        Idel = 0,
+        Idle = 0,
         Walk,
         Run,
         Jump,
