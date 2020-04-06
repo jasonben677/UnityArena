937f8bda725a8be58a4396376ca8fa082e7b7d17
BiNG0602 <kusoandy@gmail.com>
2020-04-06 15:24:49 +0800

    camera 調整
    已解決飄到無限遠問題，碰撞遮蔽還在調整中

--- Main/Assets/Script/PlayerHandle/CameraController.cs
+++ Main/Assets/Script/PlayerHandle/CameraController.cs
@@ -9,6 +9,7 @@ public class CameraController : MonoBehaviour
     public float verticalSpeed = 100.0f;
     //public float cameraDampValue = 0.01f;
 
+    private Transform pos;
     private GameObject playerHandle; //camera水平旋轉操控軸心
     private GameObject cameraHandle; //camera垂直旋轉操控軸心
     private float tempEulerX;
@@ -16,17 +17,21 @@ public class CameraController : MonoBehaviour
     private Camera camera;
 
     //private Vector3 cameraDampvelocity;
+    
+    private float offsetMagnitude;
     private bool rayTerrain;
     RaycastHit rayHit;
 
     // Start is called before the first frame update
     void Awake()
     {
+        pos = transform;
         cameraHandle = transform.parent.gameObject;
         playerHandle = cameraHandle.transform.parent.gameObject;
         tempEulerX = 20;
         model = playerHandle.GetComponent<ActorController>().model;
-        camera = Camera.main;
+        camera = Camera.main;        
+        offsetMagnitude = (cameraHandle.transform.position - transform.position).magnitude;
     }
 
     // Update is called once per frame
@@ -43,24 +48,21 @@ public class CameraController : MonoBehaviour
             CameraMovement();
         }
         else
-        {
+        {            
             CameraMovement();
         }
                        
     }
 
-    //private float offsetMagnitude;
+    
     private bool CameraRay()
     {
         Vector3 handleToPos = transform.position - cameraHandle.transform.position;
-                
-        if(Physics.Raycast(cameraHandle.transform.position, handleToPos, out rayHit, 100.0f, LayerMask.GetMask("Terrain")))
+        if (Physics.Raycast(cameraHandle.transform.position, handleToPos, out rayHit, offsetMagnitude, LayerMask.GetMask("Terrain")))
         {
             Vector3 newPos = rayHit.point - cameraHandle.transform.position;
             if(newPos.magnitude < handleToPos.magnitude) //如果障礙物擋在中間
-            {
-                //this.transform.position = rayHit.point;
-                //offsetMagnitude = newPos.magnitude;
+            {                
                 rayTerrain = true;
             }
         }
@@ -70,24 +72,23 @@ public class CameraController : MonoBehaviour
         }
         return rayTerrain;
     }
-
+    
     private void CameraMovement()
-    {         
+    {
+        Vector3 currentPos = pos.position;
+        transform.position = currentPos;
         Vector3 tempModelEuler = model.transform.eulerAngles;
 
         playerHandle.transform.Rotate(Vector3.up, pi.Jright * horizontalSpeed * Time.fixedDeltaTime);
         tempEulerX -= pi.Jup * verticalSpeed * Time.fixedDeltaTime;
         tempEulerX = Mathf.Clamp(tempEulerX, -20, 30); //限制camera上下的旋轉角度
-        cameraHandle.transform.localEulerAngles = new Vector3(tempEulerX, 0, 0);
-        //cameraHandle.transform.Rotate(Vector3.right, pi.Jup * verticalSpeed * Time.fixedDeltaTime);      
+        cameraHandle.transform.localEulerAngles = new Vector3(tempEulerX, 0, 0);        
         
-        model.transform.eulerAngles = tempModelEuler; //模型不會再跟著攝影機旋轉了~
-
-        //讓camera追空物件，有延遲視覺效果
-        camera.transform.position = Vector3.Lerp(camera.transform.position, transform.position, 3.0f * Time.fixedDeltaTime);
+        model.transform.eulerAngles = tempModelEuler; //模型不會再跟著攝影機旋轉了~ 
+        
+        camera.transform.position = Vector3.Lerp(camera.transform.position, transform.position, 2.5f * Time.fixedDeltaTime);
         //camera.transform.position = Vector3.SmoothDamp(camera.transform.position, transform.position, ref cameraDampvelocity, cameraDampValue);
         //camera.transform.eulerAngles = transform.eulerAngles;
         camera.transform.LookAt(cameraHandle.transform);
-        
     }
 }
