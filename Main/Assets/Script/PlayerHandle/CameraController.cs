ec8b952ef7d8ffd72dedd248ae257c0d55614de1
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-04-08 02:03:21 +0800

    camera待修
    微調...暫時先把遮蔽判斷註解，遮蔽判斷後的跟隨有問題，還在調整中...

--- Main/Assets/Script/PlayerHandle/CameraController.cs
+++ Main/Assets/Script/PlayerHandle/CameraController.cs
@@ -8,8 +8,7 @@ public class CameraController : MonoBehaviour
     public float horizontalSpeed = 100.0f;
     public float verticalSpeed = 100.0f;
     //public float cameraDampValue = 0.01f;
-
-    private Transform pos;
+        
     private GameObject playerHandle; //camera水平旋轉操控軸心
     private GameObject cameraHandle; //camera垂直旋轉操控軸心
     private float tempEulerX;
@@ -18,20 +17,20 @@ public class CameraController : MonoBehaviour
 
     //private Vector3 cameraDampvelocity;
     
-    private float offsetMagnitude;
+    private Vector3 dir; //射線方向
+    private float offset; //初設的距離
     private bool rayTerrain;
     RaycastHit rayHit;
 
     // Start is called before the first frame update
     void Awake()
-    {
-        pos = transform;
+    {        
         cameraHandle = transform.parent.gameObject;
         playerHandle = cameraHandle.transform.parent.gameObject;
         tempEulerX = 20;
         model = playerHandle.GetComponent<ActorController>().model;
-        camera = Camera.main;        
-        offsetMagnitude = (cameraHandle.transform.position - transform.position).magnitude;
+        camera = Camera.main;       
+        offset = (transform.position - cameraHandle.transform.position).magnitude;
     }
 
     // Update is called once per frame
@@ -41,52 +40,49 @@ public class CameraController : MonoBehaviour
     }
 
     void FixedUpdate()
-    {        
-        if(rayTerrain == true)
-        {
-            transform.position = rayHit.point;
-            CameraMovement();
-        }
-        else
-        {            
-            CameraMovement();
-        }
-                       
+    {
+        //if (rayTerrain == true)
+        //{
+        //    transform.position = rayHit.point;
+        //    CameraMovement();
+        //}
+        //else //應該是這裡有問題，空物件(cameraPos)該怎麼回到初設的距離位置呢......
+        //{            
+        //    transform.position = cameraHandle.transform.position + dir.normalized * offset;
+        //    CameraMovement();
+        //}
+        CameraMovement();
     }
 
-    
-    private bool CameraRay()
+
+    private bool CameraRay() //障礙物遮蔽判斷
     {
-        Vector3 handleToPos = transform.position - cameraHandle.transform.position;
-        if (Physics.Raycast(cameraHandle.transform.position, handleToPos, out rayHit, offsetMagnitude, LayerMask.GetMask("Terrain")))
+        dir = transform.position - cameraHandle.transform.position; 
+        Physics.Raycast(cameraHandle.transform.position, dir, out rayHit, offset, LayerMask.GetMask("Terrain"));
+        Vector3 newOffset = rayHit.point - cameraHandle.transform.position; //等等用來表示新的距離
+        if (newOffset.magnitude < offset) //如果有障礙物夾在中間
         {
-            Vector3 newPos = rayHit.point - cameraHandle.transform.position;
-            if(newPos.magnitude < handleToPos.magnitude) //如果障礙物擋在中間
-            {                
-                rayTerrain = true;
-            }
+            rayTerrain = true;
         }
         else
         {
-            rayTerrain =  false;
+            rayTerrain = false;
         }
         return rayTerrain;
     }
-    
-    private void CameraMovement()
-    {
-        Vector3 currentPos = pos.position;
-        transform.position = currentPos;
+
+    private void CameraMovement() //camera的旋轉控制，追隨空物件(cameraPos)設定
+    {        
         Vector3 tempModelEuler = model.transform.eulerAngles;
 
         playerHandle.transform.Rotate(Vector3.up, pi.Jright * horizontalSpeed * Time.fixedDeltaTime);
         tempEulerX -= pi.Jup * verticalSpeed * Time.fixedDeltaTime;
-        tempEulerX = Mathf.Clamp(tempEulerX, -20, 30); //限制camera上下的旋轉角度
+        tempEulerX = Mathf.Clamp(tempEulerX, -20, 30); 
         cameraHandle.transform.localEulerAngles = new Vector3(tempEulerX, 0, 0);        
         
-        model.transform.eulerAngles = tempModelEuler; //模型不會再跟著攝影機旋轉了~ 
+        model.transform.eulerAngles = tempModelEuler; //模型不會再跟著攝影機水平旋轉
         
-        camera.transform.position = Vector3.Lerp(camera.transform.position, transform.position, 2.5f * Time.fixedDeltaTime);
+        camera.transform.position = Vector3.Lerp(camera.transform.position, transform.position, 2.6f * Time.fixedDeltaTime);
         //camera.transform.position = Vector3.SmoothDamp(camera.transform.position, transform.position, ref cameraDampvelocity, cameraDampValue);
         //camera.transform.eulerAngles = transform.eulerAngles;
         camera.transform.LookAt(cameraHandle.transform);
