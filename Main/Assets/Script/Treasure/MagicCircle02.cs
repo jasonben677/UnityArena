0e856c8f5dfd0832e6f307c712d995e0d6687eed
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-05-14 22:59:37 +0800

    第二個寶藏設置點觸發

---
+++ Main/Assets/Script/Treasure/MagicCircle02.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircle02 : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem skillParticle;
+    [SerializeField]
+    private ParticleSystem magicCircle02;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(skillParticle != null)
+        {
+            Destroy(skillParticle.transform.parent.gameObject, 1f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        magicCircle02.Play();
+        Destroy(magicCircle02.transform.gameObject, 1.5f);
+    }
+}
