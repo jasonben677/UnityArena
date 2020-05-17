c5822febd953a11e8d9891a1bb31e5484296c1c9
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-05-17 22:26:34 +0800

    調整武器升級script，滑鼠靈敏度微調

--- Main/Assets/Script/PlayerHandle/WeaponController.cs
+++ Main/Assets/Script/PlayerHandle/WeaponController.cs
@@ -6,26 +6,28 @@ public class WeaponController : MonoBehaviour
 {
     public WeaponManager wm;
     public WeaponData wdata;
-    public static bool weaponLevelUp = false;    
+    public static bool weaponLevelUp = false;
+    private float initialATK;
 
     // Start is called before the first frame update
     void Start()
     {
         wdata = GetComponentInChildren<WeaponData>();
+        initialATK = wdata.ATK;
     }   
 
     public float GetATK()
     {
         if (weaponLevelUp == false)
         {
-            wdata.ATK *= 1f;
+            wdata.ATK = initialATK;
         }
         else 
         {
             //Debug.Log("weaponLevelUp!!!!!");
-            wdata.ATK *= 1.1f;
+            wdata.ATK = initialATK * 1.3f;
         }
-        float totalATK = Mathf.Round((this.wdata.ATK + wm.am.sm.ATK) * Random.Range(0.2f, 1f));          
+        float totalATK = Mathf.Round((wdata.ATK + wm.am.sm.ATK) * Random.Range(0.2f, 1f));          
         return totalATK;
     }
 
