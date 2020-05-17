f83540426b4601961289bf9a517a85a73fde3962
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-05-17 18:06:09 +0800

    武器升級觸發功能完成

---
+++ Main/Assets/MagicCircleStageTrigger.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicCircleStageTrigger : MonoBehaviour
+{
+    private ParticleSystem magicCircleFX;
+
+    [SerializeField] private GameObject treasureFX;
+    private float treasureStartHeight;
+        
+    private GameObject weaponShineFX;
+    private GameObject shockFX;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        magicCircleFX = GetComponentInChildren<ParticleSystem>();
+        //Debug.Log(magicCircleFX.gameObject.name);
+        treasureStartHeight = treasureFX.transform.position.y;
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") 
+        {
+            if (magicCircleFX != null) 
+            {
+                Destroy(magicCircleFX.gameObject, 0.3f);
+            }
+            WeaponController.weaponLevelUp = true;
+            CreateShockFX();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            DropTreasure();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") 
+        {
+            //生成武器發光特效
+            GameObject whR = other.gameObject.GetComponent<ActorManager>().wm.whR;
+            GameObject prefabFX = Resources.Load("WeaponShineFX") as GameObject;
+            weaponShineFX = GameObject.Instantiate(prefabFX);
+            weaponShineFX.transform.parent = whR.transform;
+            weaponShineFX.transform.localPosition = Vector3.zero;
+            weaponShineFX.transform.localRotation = Quaternion.identity;
+        }                
+    }
+    
+
+    private void CreateShockFX() 
+    {
+        //Debug.Log("shock!!!!");
+        GameObject prefabFX = Resources.Load("ShockFX") as GameObject;
+        shockFX = GameObject.Instantiate(prefabFX);
+        shockFX.transform.parent = transform;
+        shockFX.transform.localPosition = Vector3.zero;
+        shockFX.transform.localRotation = Quaternion.identity;
+    }
+
+    public void DropTreasure()
+    {
+        if (treasureFX.transform.gameObject != null)
+        {
+            if (treasureFX.transform.position.y >= treasureStartHeight - 4f)
+            {
+                treasureFX.transform.position = new Vector3(treasureFX.transform.position.x, treasureFX.transform.position.y - 0.1f, treasureFX.transform.position.z);
+            }
+            else 
+            {
+                Destroy(treasureFX.transform.gameObject, 2f);
+            }
+        }
+    }
+}
--- Main/Assets/Script/PlayerHandle/WeaponController.cs
+++ Main/Assets/Script/PlayerHandle/WeaponController.cs
@@ -6,17 +6,27 @@ public class WeaponController : MonoBehaviour
 {
     public WeaponManager wm;
     public WeaponData wdata;
+    public static bool weaponLevelUp = false;    
 
     // Start is called before the first frame update
     void Start()
     {
         wdata = GetComponentInChildren<WeaponData>();
-    }
+    }   
 
     public float GetATK()
     {
-        float totalATK = Mathf.Round((wdata.ATK + wm.am.sm.ATK) * Random.Range(0.2f, 1f));
-        return totalATK ;
+        if (weaponLevelUp == false)
+        {
+            wdata.ATK *= 1f;
+        }
+        else 
+        {
+            //Debug.Log("weaponLevelUp!!!!!");
+            wdata.ATK *= 1.5f;
+        }
+        float totalATK = Mathf.Round((this.wdata.ATK + wm.am.sm.ATK) * Random.Range(0.2f, 1f));          
+        return totalATK;
     }
 
     //// Update is called once per frame
