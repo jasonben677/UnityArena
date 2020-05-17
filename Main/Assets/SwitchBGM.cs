6b432e9820552a12ad08d7974fde1e833726be15
BiNG0602 <61902029+BiNG0602@users.noreply.github.com>
2020-05-17 11:09:56 +0800

    測試BGM切換

---
+++ Main/Assets/SwitchBGM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBGM : MonoBehaviour
+{
+
+    public AudioClip[] audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<AudioSource>().clip = audios[0];
+        this.GetComponent<AudioSource>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
