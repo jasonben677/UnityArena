9c468c9454f63edca9df4ca8e3272ff58b93ccaf
BiNG0602 <kusoandy@gmail.com>
2020-05-18 17:20:46 +0800

    測試deadShader

---
+++ Main/Assets/Script/TryDeadShader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryDeadShader : MonoBehaviour
+{
+    [SerializeField]
+    private Material dead;
+    private SkinnedMeshRenderer[] smrList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smrList = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            foreach (var smr in smrList)
+            {
+                smr.material = dead;
+            }
+        }
+    }
+}
